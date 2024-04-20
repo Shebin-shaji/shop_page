a08705c67fb5bae36594de2dac2fe2611193f8dd
Shebin-shaji <shebinshaji2615@gmail.com>
2024-04-20 21:29:57 +0530

    first commit

--- lib/widgets/custom_bottom_nav_bar.dart
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -1,20 +1,21 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 
 class CustomBottomNavBar extends StatelessWidget {
   const CustomBottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(height: 75,
+    return SizedBox(
+      height: 75,
       child: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: Padding(
-              padding: EdgeInsets.only(left: 15,right: 15,top: 4,bottom: 2),
+              padding: EdgeInsets.only(left: 15, right: 15, top: 4, bottom: 2),
               child: Icon(
                 Icons.home,
                 size: 30,
@@ -48,9 +49,7 @@ class CustomBottomNavBar extends StatelessWidget {
                 color: Colors.black45,
               ),
               label: "Profile"),
-              
         ],
-      
       ),
     );
   }
